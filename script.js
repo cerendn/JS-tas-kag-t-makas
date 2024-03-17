f0db44f2b33ff0f6f32a83afdac8ca14fbef8477
cerendn <cerendncer@outlook.com>
2024-03-17 15:34:27 +0300

    update

--- script.js
+++ script.js
@@ -13,4 +13,29 @@ let playScor= {
 let adminScor = playScor[kullaniciSecim][pcSecim];
 console.log(adminScor);
 
+let successScor = playScor[kullaniciSecim][pcSecim];
+console.log(successScor);
+
+let kayitliResimler ={
+    "tas":document.getElementById("tas").src,
+    "kagit" :document.getElementById("kagit").src,
+    "makas":document.getElementById("makas").src
+};
+
+document.getElementById("tas").remove();
+document.getElementById("kagit").remove();
+document.getElementById("makas").remove();
+
+let kullaniciResim = document.createElement("img");
+let pcResim = document.createElement("img");
+let resultMessage=document.createElement("div");
+
+kullaniciResim.src=kayitliResimler[kullaniciSecim];
+pcResim.src=kayitliResimler[pcSecim];
+
+document.getElementById("container").appendChild(kullaniciResim);
+document.getElementById("container").appendChild(resultMessage);
+document.getElementById("container").appendChild(pcResim);
+
 }
+
