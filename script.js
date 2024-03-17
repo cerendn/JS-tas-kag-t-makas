88300b2e7ad42de8102aa9e429ea106fad0642ed
cerendn <cerendncer@outlook.com>
2024-03-17 15:10:27 +0300

    playScor

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ let randomNum = Math.floor(Math.random()*3);
 let pcSecim = ["tas","kagit","makas"][randomNum];
 console.log(pcSecim);
 
+let playScor= {
+    "tas" : {"makas":1,"tas" : 0.5,"kagit":0},
+    "kagit" : {"tas":1,"kagit":0.5,"makas":0},
+    "makas" : {"kagit":1,"makas":0.5,"tas":0}
+};
+let adminScor = playScor[kullaniciSecim][pcSecim];
+console.log(adminScor);
+
 }
